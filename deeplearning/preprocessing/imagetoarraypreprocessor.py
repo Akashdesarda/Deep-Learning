8a09d161dceee8edbcb2104265551ca9c226a85e
Akash Desarda <desardaakash@gmail.com>
2019-07-12 13:06:32 +0530

    First commit

---
+++ deeplearning/preprocessing/imagetoarraypreprocessor.py
@@ -0,0 +1,18 @@
+# Importing necessary packages
+from keras.preprocessing.image import img_to_array
+
+
+class ImageToArrayPreprocessor:
+    def __init__(self, dataFormat=None):
+        self.dataFormat = dataFormat
+
+    def preprocess(self, image):
+        """Takes an image to convert it into numpy array and apply the Keras utility function that correctly rearranges
+
+        Args:
+            image: Can be path of image
+
+        Returns:
+            np.array: np.array of image
+        """
+        return img_to_array(image, dataFormat=self.dataFormat)
