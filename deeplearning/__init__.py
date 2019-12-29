893c82348482c2809988d689262e877781c68317
Akash Desarda <desardaakash@gmail.com>
2019-12-29 12:21:42 +0530

    Remove redendancy

--- deeplearning/__init__.py
+++
@@ -1,6 +0,0 @@
-from .callbacks import SaveModel
-from .callbacks import TrainingMonitor
-from .datasets import SimpleDatasetLoader
-from .nn.conv import ShallowNet, MinivVGGNet
-from .preprocessing import ImageToArrayPreprocessor
-from .preprocessing import SimplePreprocessor
