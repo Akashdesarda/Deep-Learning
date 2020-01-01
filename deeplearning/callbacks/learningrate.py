7feff04cf652f50af942189addcf8d7b1fb78a2a
Akash Desarda <desardaakash@gmail.com>
2020-01-01 21:43:18 +0530

    step decay lr with custom value

--- deeplearning/callbacks/learningrate.py
+++ deeplearning/callbacks/learningrate.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-def step_decay(epoch):
+def step_decay(epoch, initAlpha: float=0.01, factor: float=0.25, dropEvery: int=5):
+    """Step decay Learning rate
     
-    # Initialize base learning rate drop factor and epochs to drop every
-    initAlpha = 0.01
-    factor = 0.25
-    dropEvery = 5 
-    
+    Arguments:
+        initAlpha {float} -- To initialize learning rate
+        factor {float} -- Factor by which drop it
+        dropEvery {int} -- Drop every epoch no
+    """
     # compute learning rate for the current epoch 
     rate = initAlpha * (factor ** np.floor(1 + epoch)/dropEvery)
+    print(f"Using Step Decay Learning rate with specs as initAlpha: {initAlpha}, factor: {factor}, dropEvery:{dropEvery}")
     return float(rate)
